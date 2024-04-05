1b7824ae11e6b12a52808694def87c604eb12844
georgitinchev <g.tinchev@student.fontys.nl>
2024-04-05 12:01:15 +0200

    Updated UML post feedback & updated connection string and structure

---
+++ Data Access Code Layer/DataAccessLibrary/BaseDAL.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace DataAccessLibrary
+{
+    public class BaseDAL
+    {
+        private string connectionString = "Server=mssqlstud.fhict.local;Database=dbi524441_streamsage;User Id=dbi524441_streamsage;Password=e9999619;TrustServerCertificate=true;";
+
+        public BaseDAL() { }
+
+        public void CreateConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            connection.ConnectionString = connectionString;
+        }
+    }
+}
--- Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
+++ Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
@@ -1,54 +1,22 @@
 ﻿using LogicClassLibrary.Entities;
 using DataAccessLibrary;
 using System.Data;
+using LogicClassLibrary.Managers;
 
 namespace LogicClassLibrary
 {
     public class BackendService
     {
-        private DatabaseOperations databaseOperations = new DatabaseOperations();
-        public List<Movie> GetMovies()
+        private UserManager userManager;
+        private MovieManager movieManager;
+        private ReviewManager reviewManager;
+        private InterpretationManager interpretationManager;
+        public BackendService()
         {
-            var dataTable = databaseOperations.GetMovies();
-            var movies = new List<Movie>();
-
-            foreach (DataRow row in dataTable.Rows)
-            {
-                var movie = new Movie
-                {
-                    Id = (int)row["MovieID"],
-                    Title = row["Title"].ToString(),
-                    Year = (DateTime)row["ReleaseDate"],
-                    Description = row["Description"].ToString(),
-                    PosterImageURL = row["PosterImageURL"].ToString(),
-                    TrailerURL = row["TrailerURL"].ToString(),
-                };
-                movies.Add(movie);
-            }
-            return movies;
+            userManager = new UserManager();
+            movieManager = new MovieManager();
+            reviewManager = new ReviewManager();
+            interpretationManager = new InterpretationManager();
         }
-
-        // method that gathers all users
-        //public List<User> GetUsers()
-        //{
-        //    var dataTable = databaseOperations.GetUsers();
-        //    var users = new List<User>();
-
-        //    foreach (DataRow row in dataTable.Rows)
-        //    {
-        //        var user = new User
-        //        {
-        //            UserID = (int)row["UserID"],
-        //            Username = row["Username"].ToString(),
-        //            Email = row["Email"].ToString(),
-        //            PasswordHash = row["PasswordHash"].ToString(),
-        //            FirstName = row["FirstName"].ToString(),
-        //            LastName = row["LastName"].ToString(),
-        //            Settings = row["Settings"].ToString()
-        //        };
-        //        users.Add(user);
-        //    }
-        //    return users;
-        //}
     }
 }
