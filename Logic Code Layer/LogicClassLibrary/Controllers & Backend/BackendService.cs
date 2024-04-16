da6dac21b861c227a32cb45cf7d98042a6163ace
georgitinchev <g.tinchev@student.fontys.nl>
2024-04-16 16:55:13 +0200

    Altered backend service

--- Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
+++ Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
@@ -19,6 +19,7 @@ namespace LogicClassLibrary
         private InterpretationDAL interpretationDAL;
         public BackendService()
         {
+            string connectionString = "Server=mssqlstud.fhict.local;Database=dbi524441_streamsage;User Id=dbi524441_streamsage;Password=REMOVED_PASSWORD;TrustServerCertificate=true;";
             userManager = new UserManager();
             movieManager = new MovieManager();
             reviewManager = new ReviewManager();
