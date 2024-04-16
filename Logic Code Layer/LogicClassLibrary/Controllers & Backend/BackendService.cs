a4aca5212df90c373d923dc1f1ea300aaf4352e0
georgitinchev <g.tinchev@student.fontys.nl>
2024-04-16 22:45:05 +0200

    Backend Service implements Dependency Inversion

--- Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
+++ Logic Code Layer/LogicClassLibrary/Controllers & Backend/BackendService.cs
@@ -20,15 +20,40 @@ namespace LogicClassLibrary
         public BackendService()
         {
             string connectionString = "Server=mssqlstud.fhict.local;Database=dbi524441_streamsage;User Id=dbi524441_streamsage;Password=e9999619;TrustServerCertificate=true;";
-            userManager = new UserManager();
-            movieManager = new MovieManager();
-            reviewManager = new ReviewManager();
-            interpretationManager = new InterpretationManager();
-            recommendationManager = new RecommendationManager();
             movieDAL = new MovieDAL(connectionString);
             userDAL = new UserDAL(connectionString);
             reviewDAL = new ReviewDAL(connectionString);
             interpretationDAL = new InterpretationDAL(connectionString);
+            movieManager = new MovieManager(movieDAL);
+            userManager = new UserManager(userDAL);
+            reviewManager = new ReviewManager(reviewDAL);
+            interpretationManager = new InterpretationManager(interpretationDAL);
+            recommendationManager = new RecommendationManager();
         }
+
+        public Movie GetMovie(int id)
+        {
+            return movieManager.ReadMovie(id);
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            movieManager.CreateMovie(movie);
+		}
+
+		public void UpdateMovie(Movie movie)
+        {
+			movieManager.UpdateMovie(movie);
+		}
+
+		public void DeleteMovie(int id)
+        {
+			movieManager.DeleteMovie(id);
+        }
+		
+        public List<Movie> GetAllMovies()
+        {
+            return movieManager.movies;
+		}
     }
 }
