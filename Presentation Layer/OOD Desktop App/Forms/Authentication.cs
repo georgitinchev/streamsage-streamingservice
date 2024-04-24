c3a626b5357196cfdac6070baaffe8bdc1a36d3c
georgitinchev <g.tinchev@student.fontys.nl>
2024-04-24 14:11:31 +0200

    Auth Form Minor Refactor

--- Presentation Layer/OOD Desktop App/Forms/Authentication.cs
+++ Presentation Layer/OOD Desktop App/Forms/Authentication.cs
@@ -13,51 +13,53 @@ using System.Windows.Forms;
 
 namespace DesktopApp.Forms
 {
-	public partial class Authentication : Form
-	{
-		private DesktopController _desktopController;
-		public Authentication()
-		{
-			InitializeComponent();
-			CustomizeButton(loginBtn);
-			_desktopController = new DesktopController();
-		}
+    public partial class Authentication : Form
+    {
+        private DesktopController _desktopController;
+        public Authentication()
+        {
+            InitializeComponent();
+            CustomizeButton(loginBtn);
+            _desktopController = new DesktopController();
+        }
 
-		private void CustomizeButton(Button button)
-		{
-			button.FlatStyle = FlatStyle.Flat;
-			button.BackColor = Color.FromArgb(50, 50, 50);
-			button.ForeColor = Color.White;
-			button.Padding = new Padding(10);
-			GraphicsPath path = new GraphicsPath();
-			Rectangle rect = button.ClientRectangle;
-			int radius = 20;
-			path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
-			path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
-			path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
-			path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
-			path.CloseFigure();
-			button.Region = new Region(path);
-		}
+        private void CustomizeButton(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.BackColor = Color.FromArgb(50, 50, 50);
+            button.ForeColor = Color.White;
+            button.Padding = new Padding(10);
+            GraphicsPath path = new GraphicsPath();
+            Rectangle rect = button.ClientRectangle;
+            int radius = 20;
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            button.Region = new Region(path);
+        }
 
-		private void loginBtn_Click(object sender, EventArgs e)
-		{
-			bool isAuthenticated = _desktopController.loginUser(userNameLoginTextBox.Text, passwordLoginTextBox.Text);
-			if (isAuthenticated)
-			{
-				AdminDashboard adminDashboard = new AdminDashboard(userInterfaceImplementation);
-				Program.SwitchToForm(adminDashboard); 
-			}
-			else
-			{
-				MessageBox.Show("Invalid credentials");
-			}
-		}
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            bool isAuthenticated = _desktopController.loginUser(userNameLoginTextBox.Text, passwordLoginTextBox.Text);
+            if (isAuthenticated)
+            {
+                AdminDashboard adminDashboard = new AdminDashboard(_desktopController);
+                Program.SwitchToForm(adminDashboard);
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials");
+            }
+            ClearInputs();
+        }
 
-		private void ClearInputs()
-		{
-			userNameLoginTextBox.Text = "";
-			passwordLoginTextBox.Text = "";
-		}
-	}
+
+        private void ClearInputs()
+        {
+            userNameLoginTextBox.Text = "";
+            passwordLoginTextBox.Text = "";
+        }
+    }
 }
