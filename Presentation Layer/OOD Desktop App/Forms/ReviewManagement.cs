deea55e136be833149f4f1ac4e9e6cc29edf67e5
georgitinchev <g.tinchev@student.fontys.nl>
2024-04-26 10:22:35 +0200

    ReviewMgrPassthrough

--- Presentation Layer/OOD Desktop App/Forms/ReviewManagement.cs
+++ Presentation Layer/OOD Desktop App/Forms/ReviewManagement.cs
@@ -12,8 +12,10 @@ namespace DesktopApp.Forms
 {
 	public partial class ReviewManagement : Form
 	{
-		public ReviewManagement()
+		public DesktopController desktopController { get; private set; }
+		public ReviewManagement(DesktopController _desktopController)
 		{
+			this.desktopController = _desktopController;
 			InitializeComponent();
 		}
 	}
