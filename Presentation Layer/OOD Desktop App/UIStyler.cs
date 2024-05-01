a7df68fee38b193b318813cfbc442ef10960882e
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-01 22:07:36 +0300

    Class encompassing Datagrid & UI Style logic

---
+++ Presentation Layer/OOD Desktop App/UIStyler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp
+{
+    public static class UIStyler
+    {
+        public static void StyleButtonColumns(DataGridView dataGridView)
+        {
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                if (column is DataGridViewButtonColumn buttonColumn)
+                {
+                    buttonColumn.FlatStyle = FlatStyle.Flat;
+                    buttonColumn.DefaultCellStyle.BackColor = Color.LightGray;
+                    buttonColumn.DefaultCellStyle.ForeColor = Color.Black;
+                    buttonColumn.DefaultCellStyle.SelectionBackColor = Color.DarkGray;
+                    buttonColumn.DefaultCellStyle.SelectionForeColor = Color.White;
+                    buttonColumn.DefaultCellStyle.Padding = new Padding(5);
+                    buttonColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                }
+            }
+        }
+        public static void StyleDataGridView(DataGridView dataGridView)
+        {
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.BackgroundColor = Color.White;
+            dataGridView.BorderStyle = BorderStyle.None;
+            dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+            dataGridView.EnableHeadersVisualStyles = false;
+            dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.Gray;
+            dataGridView.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView.RowTemplate.Height = 50;
+        }
+        public static void AddButtonColumn(DataGridView dataGridView, string columnName, string buttonText)
+        {
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn
+            {
+                Name = columnName,
+                Text = buttonText,
+                UseColumnTextForButtonValue = true,
+                Width = 50
+            };
+            dataGridView.Columns.Add(buttonColumn);
+        }
+    }
+}
