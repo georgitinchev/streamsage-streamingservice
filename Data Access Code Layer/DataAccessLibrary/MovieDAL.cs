5472e3f3a005bcc99650bf69c83fcccac4434ade
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-02 14:42:35 +0300

    MovieDAL modified to handle Genres

--- Data Access Code Layer/DataAccessLibrary/MovieDAL.cs
+++ Data Access Code Layer/DataAccessLibrary/MovieDAL.cs
@@ -18,20 +18,22 @@ namespace DataAccessLibrary
             {
             }
 
+            // Method to read all movies
             public List<MovieDTO> ReadAllMovies()
             {
-                string query = "SELECT ID, Title, ReleaseDate, Description, PosterImageURL, TrailerURL, RuntimeMinutes, AverageRating FROM Movie";
-                List<MovieDTO> movies = new List<MovieDTO>();
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                var movies = new List<MovieDTO>();
+                var movieQuery = "SELECT ID, Title, ReleaseDate, Description, PosterImageURL, TrailerURL, RuntimeMinutes, AverageRating FROM Movie";
+                var genreQuery = "SELECT mg.MovieID, g.Name FROM MovieGenre mg INNER JOIN Genre g ON mg.GenreID = g.ID";
+                using (var connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    using (var command = new SqlCommand(movieQuery, connection))
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        using (var reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                MovieDTO movie = new MovieDTO
+                                movies.Add(new MovieDTO
                                 {
                                     Id = reader.GetInt32(0),
                                     Title = reader.GetString(1),
@@ -40,17 +42,31 @@ namespace DataAccessLibrary
                                     PosterImageURL = reader.IsDBNull(4) ? null : reader.GetString(4),
                                     TrailerURL = reader.IsDBNull(5) ? null : reader.GetString(5),
                                     RuntimeMinutes = reader.GetInt32(6),
-                                    AverageRating = reader.IsDBNull(7) ? null : reader.GetDecimal(7)
-                                };
-                                movies.Add(movie);
+                                    AverageRating = reader.IsDBNull(7) ? null : reader.GetDecimal(7),
+                                    Genres = new List<string>()
+                                });
                             }
-
-
+                        }
+                    }
+                    using (var command = new SqlCommand(genreQuery, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int movieId = reader.GetInt32(0);
+                                string genreName = reader.GetString(1);
+                                var movie = movies.FirstOrDefault(m => m.Id == movieId);
+                                movie?.Genres.Add(genreName);
+                            }
                         }
                     }
                 }
                 return movies;
             }
+
+            // Method to update a movie
+            // Method to update a movie
             public void UpdateMovie(MovieDTO movie)
             {
                 string query = "UPDATE Movie SET Title = @Title, ReleaseDate = @ReleaseDate, Description = @Description, PosterImageURL = @PosterImageURL, TrailerURL = @TrailerURL, RuntimeMinutes = @RuntimeMinutes, AverageRating = @AverageRating WHERE ID = @Id";
@@ -58,22 +74,40 @@ namespace DataAccessLibrary
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    using (SqlTransaction transaction = connection.BeginTransaction())
                     {
-                        command.Parameters.AddWithValue("@Id", movie.Id);
-                        command.Parameters.AddWithValue("@Title", movie.Title);
-                        command.Parameters.AddWithValue("@ReleaseDate", movie.ReleaseDate);
-                        command.Parameters.AddWithValue("@Description", movie.Description);
-                        command.Parameters.AddWithValue("@PosterImageURL", movie.PosterImageURL);
-                        command.Parameters.AddWithValue("@TrailerURL", movie.TrailerURL);
-                        command.Parameters.AddWithValue("@RuntimeMinutes", movie.RuntimeMinutes);
-                        command.Parameters.Add("@AverageRating", SqlDbType.Decimal);
-                        command.Parameters["@AverageRating"].Value = movie.AverageRating.HasValue ? (object)movie.AverageRating.Value : DBNull.Value;
-                        command.ExecuteNonQuery();
+                        try
+                        {
+                            using (SqlCommand command = new SqlCommand(query, connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@Id", movie.Id);
+                                command.Parameters.AddWithValue("@Title", movie.Title);
+                                command.Parameters.AddWithValue("@ReleaseDate", movie.ReleaseDate);
+                                command.Parameters.AddWithValue("@Description", movie.Description);
+                                command.Parameters.AddWithValue("@PosterImageURL", movie.PosterImageURL);
+                                command.Parameters.AddWithValue("@TrailerURL", movie.TrailerURL);
+                                command.Parameters.AddWithValue("@RuntimeMinutes", movie.RuntimeMinutes);
+                                command.Parameters.Add("@AverageRating", SqlDbType.Decimal);
+                                command.Parameters["@AverageRating"].Value = movie.AverageRating.HasValue ? (object)movie.AverageRating.Value : DBNull.Value;
+                                command.ExecuteNonQuery();
+                            }
+
+                            ClearMovieGenres(movie.Id, connection, transaction);
+                            InsertMovieGenres(movie.Id, movie.Genres, connection, transaction);
+
+                            transaction.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            transaction.Rollback();
+                            throw new Exception("Transaction failed: " + ex.Message);
+                        }
                     }
                 }
             }
 
+
+            // Method to delete a movie
             public void DeleteMovie(int movieId)
             {
                 string deleteMovieQuery = "DELETE FROM Movie WHERE ID = @Id";
@@ -88,7 +122,6 @@ namespace DataAccessLibrary
                             command.ExecuteNonQuery();
                         }
                     }
-                    ReseedIdentityColumn("Movie", movieId);
                 }
                 catch (Exception ex)
                 {
@@ -96,48 +129,102 @@ namespace DataAccessLibrary
                 }
             }
 
-
-            // Reseed id of deleted movie
-            private void ReseedIdentityColumn(string tableName, int seedValue)
-            {
-                try
-                {
-                    string reseedQuery = $"DBCC CHECKIDENT ('{tableName}', RESEED, {seedValue})";
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
-                        using (SqlCommand command = new SqlCommand(reseedQuery, connection))
-                        {
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception("Error reseeding identity column: " + ex.Message);
-                }
-            }
-
-
+            // Method to create a movie
             public void CreateMovie(MovieDTO movie)
             {
-                string query = "INSERT INTO Movie (Title, ReleaseDate, Description, PosterImageURL, TrailerURL, RuntimeMinutes, AverageRating) VALUES (@Title, @ReleaseDate, @Description, @PosterImageURL, @TrailerURL, @RuntimeMinutes, NULL)";
+                string query = "INSERT INTO Movie (Title, ReleaseDate, Description, PosterImageURL, TrailerURL, RuntimeMinutes, AverageRating) OUTPUT INSERTED.ID VALUES (@Title, @ReleaseDate, @Description, @PosterImageURL, @TrailerURL, @RuntimeMinutes, NULL)";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    using (SqlTransaction transaction = connection.BeginTransaction()) 
                     {
-                        command.Parameters.AddWithValue("@Title", movie.Title);
-                        command.Parameters.AddWithValue("@ReleaseDate", movie.ReleaseDate);
-                        command.Parameters.AddWithValue("@Description", movie.Description);
-                        command.Parameters.AddWithValue("@PosterImageURL", movie.PosterImageURL);
-                        command.Parameters.AddWithValue("@TrailerURL", movie.TrailerURL);
-                        command.Parameters.AddWithValue("@RuntimeMinutes", movie.RuntimeMinutes);
+                        using (SqlCommand command = new SqlCommand(query, connection, transaction)) 
+                        {
+                            command.Parameters.AddWithValue("@Title", movie.Title);
+                            command.Parameters.AddWithValue("@ReleaseDate", movie.ReleaseDate);
+                            command.Parameters.AddWithValue("@Description", movie.Description);
+                            command.Parameters.AddWithValue("@PosterImageURL", movie.PosterImageURL);
+                            command.Parameters.AddWithValue("@TrailerURL", movie.TrailerURL);
+                            command.Parameters.AddWithValue("@RuntimeMinutes", movie.RuntimeMinutes);
+                            int movieId = (int)command.ExecuteScalar();
+                            if (movie.Genres != null && movie.Genres.Count > 0)
+                            {
+                                InsertMovieGenres(movieId, movie.Genres, connection, transaction); 
+                            }
+                            transaction.Commit(); 
+                        }
+                    }
+                }
+            }
+
+            // Method to insert genres for a movie
+            private void InsertMovieGenres(int movieId, List<string> genres, SqlConnection connection, SqlTransaction transaction)
+            {
+                foreach (var genreName in genres)
+                {
+                    int genreId = GetGenreIdByName(genreName, connection, transaction); // Ensure GetGenreIdByName also supports transactions
+                    string insertQuery = "INSERT INTO MovieGenre (MovieID, GenreID) VALUES (@MovieID, @GenreID)";
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@MovieID", movieId);
+                        command.Parameters.AddWithValue("@GenreID", genreId);
                         command.ExecuteNonQuery();
                     }
                 }
             }
 
+            // Method to clear all genres for a movie
+            private void ClearMovieGenres(int movieId, SqlConnection connection, SqlTransaction transaction)
+            {
+                string deleteQuery = "DELETE FROM MovieGenre WHERE MovieID = @MovieID";
+                using (SqlCommand command = new SqlCommand(deleteQuery, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@MovieID", movieId);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Method to query genre ID by name with transaction support
+            private int GetGenreIdByName(string genreName, SqlConnection connection, SqlTransaction transaction)
+            {
+                string query = "SELECT ID FROM Genre WHERE Name = @GenreName";
+                using (SqlCommand command = new SqlCommand(query, connection, transaction)) 
+                {
+                    command.Parameters.AddWithValue("@GenreName", genreName);
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        return (int)result;
+                    }
+                    else
+                    {
+                        throw new Exception($"Genre not found: {genreName}");
+                    }
+                }
+            }
+
+
+            // Method to get all movie genres
+            public List<string> GetAllGenres()
+            {
+                List<string> genres = new List<string>();
+                string query = "SELECT Name FROM Genre";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                genres.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+                return genres;
+            }
             public MovieDTO GetMovie(int movieId)
             {
                 throw new NotImplementedException();
