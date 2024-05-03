e95dada28db4dcf745cdcf7953c4e8357cadbd67
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-03 22:56:07 +0300

    Settings Form Logic Implementing Algo Tester

--- Presentation Layer/OOD Desktop App/Forms/SettingsForm.cs
+++ Presentation Layer/OOD Desktop App/Forms/SettingsForm.cs
@@ -1,22 +1,63 @@
-﻿using System;
+﻿using LogicClassLibrary.Managers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DesktopApp.Forms
 {
-	public partial class SettingsForm : Form
-	{
-		public DesktopController desktopController { get; private set; }
-		public SettingsForm(DesktopController _desktopController)
-		{
-			InitializeComponent();
-			this.desktopController = _desktopController;
-		}
-	}
+    public partial class SettingsForm : Form
+    {
+        public DesktopController desktopController { get; private set; }
+        public SettingsForm(DesktopController _desktopController)
+        {
+            InitializeComponent();
+            this.desktopController = _desktopController;
+            PopulateUserPicker();
+            UIStyler.MakeCheckBoxesMutuallyExclusive(selectBehaviorBasedCheckBox, selectContentBasedCheckBox);
+
+        }
+
+        private async void DisplayRecommendations()
+        {
+            if (settingsUserPicker.SelectedItem == null || (!selectBehaviorBasedCheckBox.Checked && !selectContentBasedCheckBox.Checked))
+            {
+                return;
+            }
+            string username = settingsUserPicker.SelectedItem.ToString();
+            int numRecommendations = 5; // can be adjusted by user
+            // determine type of recommend based on user selection
+            var type = selectBehaviorBasedCheckBox.Checked ?
+                RecommendationManager.RecommendationType.BehaviorBased :
+                RecommendationManager.RecommendationType.ContentBased;
+
+            var recommendations = await desktopController.backendService?.recommendationManager?.RecommendMoviesForUser(username, numRecommendations, type);
+
+            settingsDisplayRecommendationsListBox.Items.Clear();
+            if (recommendations != null)
+            {
+                foreach (var movie in recommendations)
+                {
+                    settingsDisplayRecommendationsListBox.Items.Add(movie.Title);
+                }
+            }
+        }
+
+        private void settingsTestAlgorithm_Click(object sender, EventArgs e)
+        {
+            DisplayRecommendations();
+        }
+
+        private void PopulateUserPicker()
+        {
+            var userNames = desktopController.backendService?.GetAllUsers().Select(u => u.Username).ToList();
+            UIStyler.PopulateComboBox(settingsUserPicker, userNames);
+        }
+    }
 }
