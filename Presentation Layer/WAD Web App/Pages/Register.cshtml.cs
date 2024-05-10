58f67d75c47d7d4b0c00777ecfc8a0b7af0a7be2
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-10 10:20:10 +0200

    Register page

---
+++ Presentation Layer/WAD Web App/Pages/Register.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamSageWAD.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace StreamSageWAD.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private WebController webController;
+
+        public RegisterModel(WebController webController)
+        {
+            this.webController = webController;
+        }
+
+        [BindProperty]
+        public RegisterDTO registerDTO { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                bool userExists = webController.loginUser(registerDTO.Email, registerDTO.Password);
+                if (!userExists)
+                {
+                    webController.registerUser(registerDTO.Username, registerDTO.Password, registerDTO.Email, registerDTO.FirstName, registerDTO.LastName);
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, registerDTO.Email),
+                        new Claim(ClaimTypes.NameIdentifier, registerDTO.Username)
+                    };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                    return RedirectToPage("/Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User already exists.");
+                    return Page();
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, $"An unexpected error occurred during registration. {e.Message} .Please try again later.");
+                return Page();
+            }
+        }
+    }
+}
