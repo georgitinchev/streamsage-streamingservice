e04d594de26df47a811373b612d50ec367134b4e
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-10 10:19:31 +0200

    Login/Register razor models

---
+++ Presentation Layer/WAD Web App/Models/RegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreamSageWAD.Models
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string? LastName { get; set; }
+    }
+}
