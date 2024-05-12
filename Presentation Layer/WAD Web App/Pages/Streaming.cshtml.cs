6b0056576945b77f20a7acb9c47f604f8b127715
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-12 13:14:50 +0200

    search Icn

--- Presentation Layer/WAD Web App/Pages/Streaming.cshtml.cs
+++ Presentation Layer/WAD Web App/Pages/Streaming.cshtml.cs
@@ -1,20 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.ComponentModel.DataAnnotations;
+using LogicClassLibrary;
+using LogicClassLibrary.Entities; // Assuming this namespace contains the Movie class
 
 namespace StreamSageWAD.Pages
 {
-	public class StreamingModel : PageModel
-	{
-		[Required(ErrorMessage = "Trailer URL is required")]
-		public string? TrailerURL { get; set; }
-		[Required(ErrorMessage = "Description is required")]
-		public string? Description { get; set; }
-		public void OnGet(string trailerUrl, string description)
-		{
-			TrailerURL = trailerUrl;
-			Description = description;
-		}
-	}
+    [Authorize]
+    public class StreamingModel : PageModel
+    {
+        private WebController webController;
+        public StreamingModel(WebController webController)
+        {
+            this.webController = webController;
+        }
+
+        public Movie? MovieDetails { get; private set; }
+
+        public IActionResult OnGet(int movieId)
+        {
+            MovieDetails = webController.backendService.GetMovie(movieId);
+            if (MovieDetails == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
 }
