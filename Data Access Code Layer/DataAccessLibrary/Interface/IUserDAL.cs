084cdd5c991a2cfa3e2f33266d881b2cdc190a2e
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-23 13:35:44 +0200

    Movie and User DAL moved to DAL layer

---
+++ Data Access Code Layer/DataAccessLibrary/Interface/IUserDAL.cs
@@ -0,0 +1,19 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IUserDAL
+    {
+        List<UserDTO> ReadAllUsers();
+        void CreateUser(UserDTO user);
+        UserDTO GetUserByUsername(string username);
+        void UpdateUser(UserDTO user);
+        void DeleteUser(int userId);
+        void ChangePassword(string username, string newPassword);
+    }
+}
