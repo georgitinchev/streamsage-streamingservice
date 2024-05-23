52f2f370fda6a4405de98188136a88425732c89b
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-23 10:26:36 +0200

    Interface for the recommendation strategy

---
+++ Logic Code Layer/LogicClassLibrary/Interface/Algorhitmic/IRecommendationStrategy.cs
@@ -0,0 +1,14 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Interface.Algorhitmic
+{
+    public interface IRecommendationStrategy
+    {
+        List<MovieDTO> RecommendMovies(string username, List<MovieDTO> allMovies);
+    }
+}
