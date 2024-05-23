50799c051ee479cb9fb20dc10630702d98ecdd4a
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-23 13:35:44 +0200

    Movie and User DAL moved to DAL layer

---
+++ Data Access Code Layer/DataAccessLibrary/Interface/IMovieDAL.cs
@@ -0,0 +1,18 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IMovieDAL
+    {
+        void CreateMovie(MovieDTO movie);
+        MovieDTO GetMovie(int movieId);
+        void UpdateMovie(MovieDTO movie);
+        void DeleteMovie(int movieId);
+        List<MovieDTO> ReadAllMovies();
+    }
+}
