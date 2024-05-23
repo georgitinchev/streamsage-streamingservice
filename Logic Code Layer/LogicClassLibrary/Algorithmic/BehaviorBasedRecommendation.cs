ff6f63193d2c3b02374389936c5e14f470eacbd6
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-23 10:26:08 +0200

    Strategy pattern two choice classes

---
+++ Logic Code Layer/LogicClassLibrary/Algorithmic/BehaviorBasedRecommendation.cs
@@ -0,0 +1,18 @@
+﻿using DTOs;
+using LogicClassLibrary.Interface.Algorhitmic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Algorithmic
+{
+    public class BehaviorBasedRecommendation : IRecommendationStrategy
+    {
+        public List<MovieDTO> RecommendMovies(string username, List<MovieDTO> allMovies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
