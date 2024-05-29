18cb0f3b39bf2e70412ad921eb56e16338099289
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-29 12:02:53 +0200

    Pwd helper salting

--- Logic Code Layer/LogicClassLibrary/Helpers/PasswordHelper.cs
+++ Logic Code Layer/LogicClassLibrary/Helpers/PasswordHelper.cs
@@ -9,11 +9,22 @@ namespace LogicClassLibrary.Helpers
 {
     public static class PasswordHelper
     {
-        public static string HashPassword(string password)
+        public static string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string saltedPassword = password + salt;
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
