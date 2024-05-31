04cb31c6a18c8728f713c8854b1eeb5a3118599c
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-31 11:48:21 +0200

    Readme notes

--- UnitTests/WebControllerTests.cs
+++ UnitTests/WebControllerTests.cs
@@ -1,11 +1,3 @@
-using DesktopApp;
-using DTOs;
-using LogicClassLibrary;
-using LogicClassLibrary.Entities;
-using LogicClassLibrary.Managers;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
 namespace StreamSageWAD
 {
 
@@ -15,7 +7,7 @@ namespace StreamSageWAD
         [TestMethod]
         public void LoginUser_ValidCredentials_SetsLoggedInUser()
         {
-           // To be implemented after switching the webcontroller's dependency on IBackendService properly and getting rid of the direct instantiation of BackendService
+            // To be implemented after switching the webcontroller's dependency on IBackendService properly and getting rid of the direct instantiation of BackendService
         }
     }
 
--- WKS Docs/README.md
+++ WKS Docs/README.md
@@ -1,2 +1,5 @@
 Link To Repo:
 https://git.fhict.nl/I524441/individual-project-sem2
+
+Test Report
+Activity Diagram + Make the Test Cases more specific
