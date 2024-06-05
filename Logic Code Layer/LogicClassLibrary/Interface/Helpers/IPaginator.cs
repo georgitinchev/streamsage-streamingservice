059de7a900ece8a722d86e5b6afccb47faf7cba7
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-05 15:57:30 +0200

    Paginator + Paginator Manager + Interfaces for them
    Classes that handle object retrieval based on pagesize + pagenumber. This is a smart workaround to retrieving all objects in the database. Currently we have close to 3000 movies.

---
+++ Logic Code Layer/LogicClassLibrary/Interface/Helpers/IPaginator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Interface.Helpers
+{
+    public interface IPaginator<T>
+    {
+        int CurrentPage { get; }
+        int PageSize { get; set; }
+        List<T> LoadPage(int pageNumber);
+        List<T> NextPage();
+        List<T> PreviousPage();
+    }
+}
