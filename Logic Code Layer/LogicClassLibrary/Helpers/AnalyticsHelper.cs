a621dacb576f9e83ed3a461798834a690e7a44ba
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-06 22:58:23 +0200

    Analytics Helper Class addition

---
+++ Logic Code Layer/LogicClassLibrary/Helpers/AnalyticsHelper.cs
@@ -0,0 +1,49 @@
+﻿using DTOs;
+using LogicClassLibrary.Interface.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Helpers
+{
+    public static class AnalyticsHelper
+    {
+        public static double CalculateAverageRating(IMovieService movieService, int movieId)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+
+        public static List<MovieDTO> GetTopRatedMovies(IMovieService movieService)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+
+        public static List<UserDTO> GetMostActiveUsers(IUserService userService)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+
+        public static List<MovieDTO> GetMostReviewedMovies(IMovieService movieService)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+
+        public static Dictionary<DateTime, int> GetUserActivityOverTime(IUserService userService, int userId)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+
+        public static Dictionary<DateTime, int> GetMoviePopularityOverTime(IMovieService movieService, int movieId)
+        {
+            // Implementation...
+            throw new NotImplementedException();
+        }
+    }
+}
