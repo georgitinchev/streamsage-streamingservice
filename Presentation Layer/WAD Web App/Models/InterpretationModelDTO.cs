2487dd2c0081f1517b48db024afb95ce42e962bd
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-09 19:56:07 +0200

    WAD Review and Inteprretation Models

---
+++ Presentation Layer/WAD Web App/Models/InterpretationModelDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreamSageWAD.Models
+{
+    public class InterpretationModelDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+
+        [Required]
+        [StringLength(1000, ErrorMessage = "Interpretation cannot be longer than 1000 characters.")]
+        public string? InterpretationText { get; set; }
+    }
+}
---
+++ Presentation Layer/WAD Web App/Models/ReviewModelDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreamSageWAD.Models
+{
+    public class ReviewModelDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters.")]
+        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(1000, MinimumLength = 20, ErrorMessage = "Content must be between 20 and 1000 characters.")]
+        public string? Content { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10.")]
+        public int Rating { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+    }
+}
