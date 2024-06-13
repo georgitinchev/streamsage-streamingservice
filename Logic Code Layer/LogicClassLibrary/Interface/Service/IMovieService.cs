d0219840da47fe4d694acf08a5a37233c4c23df3
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:09:56 +0200

    Movie Service Interface 1.1

--- Logic Code Layer/LogicClassLibrary/Interface/Service/IMovieService.cs
+++ Logic Code Layer/LogicClassLibrary/Interface/Service/IMovieService.cs
@@ -6,13 +6,17 @@ namespace LogicClassLibrary.Interface.Service
     public interface IMovieService
     {
         List<string> GetAllGenres();
-        MovieDTO GetMovie(int id);
-        void AddMovie(MovieDTO movie);
-        void UpdateMovie(MovieDTO movieDto);
-        void DeleteMovie(int id);
-        List<MovieDTO> SearchMovies(string criteria);
         List<MovieDTO> GetAllMovies();
-        List<MovieDTO> GetFeaturedMovies();
-        List<MovieDTO> GetRandomMovies();
+        MovieDTO GetMovie(int id);
+        void AddMovie(string title, DateTime releaseDate, string description, string posterImageURL, string trailerURL, int runtimeMinutes, decimal averageRating, List<string> genres, List<string> directors, List<string> actors); // Add averageRating here
+        void UpdateMovie(int id, string title, DateTime releaseDate, string description, string posterImageURL, string trailerURL, int runtimeMinutes, decimal averageRating, List<string> genres, List<string> directors, List<string> actors);
+        void DeleteMovie(int id);
+        List<MovieDTO> GetMoviesPage(int pageNumber, int pageSize);
+        List<MovieDTO> SearchMovies(string criteria);
+        int GetTotalMovies();
+        bool MovieExists(int movieId);
+        void AddGenreToMovie(int movieId, string genre);
+        void AddActorToMovie(int movieId, string actor);
+        void AddDirectorToMovie(int movieId, string director);
     }
 }
