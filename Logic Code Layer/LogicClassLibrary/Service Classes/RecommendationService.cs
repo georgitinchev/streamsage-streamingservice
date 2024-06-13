409b54b3a05f93b65da47a60e321c22f39f16bdc
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:07:32 +0200

    Recommendation Service class now implements manager properly.

--- Logic Code Layer/LogicClassLibrary/Service Classes/RecommendationService.cs
+++ Logic Code Layer/LogicClassLibrary/Service Classes/RecommendationService.cs
@@ -1,27 +1,42 @@
 ﻿using DTOs;
+using LogicClassLibrary.Algorithmic;
 using LogicClassLibrary.Interface.Manager;
 using LogicClassLibrary.Interface.Service;
 using LogicClassLibrary.Managers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LogicClassLibrary.Service_Classes
 {
+    // Recommendation service class that takes the manager interface and the two recommendation classes
     public class RecommendationService : IRecommendationService
     {
         private IRecommendationManager recommendationManager;
+        private BehaviorBasedRecommendation behaviorBasedRecommendation;
+        private ContentBasedRecommendation contentBasedRecommendation;
 
-        public RecommendationService(IRecommendationManager recommendationManager)
+        public RecommendationService(IRecommendationManager recommendationManager, ContentBasedRecommendation contentBasedRecommendation, BehaviorBasedRecommendation behaviorBasedRecommendation)
         {
             this.recommendationManager = recommendationManager;
+            this.contentBasedRecommendation = contentBasedRecommendation;
+            this.behaviorBasedRecommendation = behaviorBasedRecommendation;
         }
 
-        public List<MovieDTO> RecommendMoviesForUser(string username, int numRecommendations, RecommendationManager.RecommendationType type)
+        // Method that recommends movies to a user based on recommendation type and number of recommendations
+        public List<MovieDTO> RecommendMoviesForUser(string username, int numRecommendations, RecommendationType type)
         {
-            return recommendationManager.RecommendMoviesForUser(username, numRecommendations, type);
+            switch (type)
+            {
+                case RecommendationType.BehaviorBased:
+                    recommendationManager.SetStrategy(behaviorBasedRecommendation);
+                    break;
+                case RecommendationType.ContentBased:
+                    recommendationManager.SetStrategy(contentBasedRecommendation);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid recommendation type.");
+            }
+            return recommendationManager.RecommendMoviesForUser(username, numRecommendations);
         }
     }
 }
