febfbcf027d3c6b40caeb55ad8ab1271108edb16
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:13:48 +0200

    DAL exception class

--- Data Access Code Layer/DataAccessLibrary/Exception/DataAccessException.cs
+++ Data Access Code Layer/DataAccessLibrary/Exception/DataAccessException.cs
@@ -1,6 +1,33 @@
-﻿namespace DataAccessLibrary.Exception
+﻿using System;
+
+namespace DataAccessLibrary.Exception
 {
-    internal class DataAccessException : System.Exception
+    public class DataAccessException : System.Exception
     {
+        public DataAccessException() : base() { }
+
+        public DataAccessException(string message) : base(message) { }
+
+        public DataAccessException(string message, System.Exception innerException) : base(message, innerException) { }
+
+        public const string DatabaseErrorMessage = "A database error occurred.";
+        public const string InvalidOperationErrorMessage = "The operation is not valid due to the current state of the object.";
+        public const string UnexpectedErrorMessage = "An unexpected error occurred.";
+
+        public static string GetDatabaseErrorMessage(string action)
+        {
+            return $"{DatabaseErrorMessage} {action}.";
+        }
+
+        public static string GetInvalidOperationErrorMessage(string action)
+        {
+            return $"{InvalidOperationErrorMessage} {action}.";
+        }
+
+        public static string GetUnexpectedErrorMessage(string action)
+        {
+            return $"{UnexpectedErrorMessage} {action}.";
+        }
     }
 }
+    
