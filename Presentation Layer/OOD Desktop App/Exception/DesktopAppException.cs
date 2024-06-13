f0434fb91851069ae08ab5629c4d6cf95c2dd742
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:05:54 +0200

    Desktop app exception class added exceptions

--- Presentation Layer/OOD Desktop App/Exception/DesktopAppException.cs
+++ Presentation Layer/OOD Desktop App/Exception/DesktopAppException.cs
@@ -1,6 +1,59 @@
-﻿namespace DesktopApp.Exception
+﻿using System;
+
+namespace DesktopApp.Exception
 {
-    internal class DesktopAppException
+    public class DesktopAppException : System.Exception
     {
+        public DesktopAppException() : base() { }
+
+        public DesktopAppException(string message) : base(message) { }
+
+        public DesktopAppException(string message, System.Exception innerException) : base(message, innerException) { }
     }
+
+    public class MovieNotFoundException : DesktopAppException
+    {
+        public MovieNotFoundException() : base() { }
+
+        public MovieNotFoundException(string message) : base(message) { }
+
+        public MovieNotFoundException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+
+    public class InvalidMovieException : DesktopAppException
+    {
+        public InvalidMovieException() : base() { }
+
+        public InvalidMovieException(string message) : base(message) { }
+
+        public InvalidMovieException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+
+    public class InvalidUserException : DesktopAppException
+    {
+        public InvalidUserException() : base() { }
+
+        public InvalidUserException(string message) : base(message) { }
+
+        public InvalidUserException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+
+    public class UserNotFoundException : DesktopAppException
+    {
+        public UserNotFoundException() : base() { }
+
+        public UserNotFoundException(string message) : base(message) { }
+
+        public UserNotFoundException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+
+    public class InvalidReviewException : DesktopAppException
+    {
+        public InvalidReviewException() : base() { }
+
+        public InvalidReviewException(string message) : base(message) { }
+
+        public InvalidReviewException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+
 }
