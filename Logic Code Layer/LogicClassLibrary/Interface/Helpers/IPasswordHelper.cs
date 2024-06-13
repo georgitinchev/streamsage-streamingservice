164dc1aa0f366b1ccb83dd31529e49eba630ca1d
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:12:01 +0200

    Password helper specific interface

---
+++ Logic Code Layer/LogicClassLibrary/Interface/Helpers/IPasswordHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Interface.Helpers
+{
+    public interface IPasswordHelper
+    {
+        string GenerateSalt();
+        string HashPassword(string password, string salt);
+        bool VerifyPassword(string password, string storedHash, string storedSalt);
+    }
+}
+
