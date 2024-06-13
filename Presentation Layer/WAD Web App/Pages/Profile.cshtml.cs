dfc2d51f853f7cf02f41f2693aea6ff2d7475a44
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:02:17 +0200

    Profile Page 1.0

--- Presentation Layer/WAD Web App/Pages/Profile.cshtml.cs
+++ Presentation Layer/WAD Web App/Pages/Profile.cshtml.cs
@@ -1,14 +1,46 @@
+using DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamSageWAD.Exception;
+using System.Security.Claims;
 
 namespace StreamSageWAD.Pages
 {
     [Authorize]
     public class ProfileModel : PageModel
     {
+        private IWebController _webController;
+        public List<MovieDTO> RecentlyWatchedMovies { get; private set; }
+        public List<MovieDTO> Watchlist { get; private set; }
+        public List<MovieDTO> Favorites { get; private set; }
+        public List<string> FavoriteGenres { get; private set; }
+        public ProfileModel(IWebController webController)
+        {
+            _webController = webController;
+            RecentlyWatchedMovies = new List<MovieDTO>();
+            Watchlist = new List<MovieDTO>();
+            Favorites = new List<MovieDTO>();
+            FavoriteGenres = new List<string>();
+        }
+
         public void OnGet()
         {
+            try
+            {
+                var user = _webController.UserService.Read(int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value));
+                RecentlyWatchedMovies = _webController.GetMoviesByIds(user.RecentlyWatchedMovieIds);
+                Watchlist = user.WatchList;
+                Favorites = user.FavoriteMovies;
+            }
+            catch (MovieNotFoundException ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+            }
+            catch (System.Exception ex)
+            {
+                TempData["ErrorMessage"] = $"An unexpected error occurred. {ex.Message}";
+            }
         }
     }
 }
