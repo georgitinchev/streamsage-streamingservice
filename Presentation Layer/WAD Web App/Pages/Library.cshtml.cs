34f5378190e4ea498556bee305496e680510994a
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:02:30 +0200

    Library page 1.0

--- Presentation Layer/WAD Web App/Pages/Library.cshtml.cs
+++ Presentation Layer/WAD Web App/Pages/Library.cshtml.cs
@@ -1,19 +1,21 @@
 using DTOs;
-using LogicClassLibrary;
-using LogicClassLibrary.Entities;
 using LogicClassLibrary.Managers;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using LogicClassLibrary.Managers;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StreamSageWAD.Pages
 {
     public class LibraryModel : PageModel
     {
-        private IWebController webController;
+        private readonly IWebController webController;
         public List<MovieDTO>? FeaturedMovies { get; private set; }
         public List<MovieDTO>? RandomMovies { get; private set; }
-        public List<MovieDTO>? UsageBasedMovies { get; private set; } 
+        public List<MovieDTO>? UsageBasedMovies { get; private set; }
+        public List<MovieDTO>? ContinueWatching { get; private set; }
         public string? ErrorMessage { get; private set; }
 
         public LibraryModel(IWebController webController)
@@ -21,18 +23,36 @@ namespace StreamSageWAD.Pages
             this.webController = webController;
         }
 
-        public async Task OnGet()
+        public void OnGet()
         {
             try
             {
-                FeaturedMovies = webController.MovieService.GetMoviesPage(4,8); // Fetch first 5 movies as featured
-                RandomMovies = webController.MovieService.GetMoviesPage(5, 8); // Fetch 5 random movies
-                UsageBasedMovies = webController.MovieService.GetMoviesPage(6, 8); // Fetch 5 movies based on user behavior
+                // Get the username of the currently logged in user or the default user
+                string username = User.Identity.Name ?? webController.GetDefaultUser();
+               // Get the movies recommended based on the content
+                FeaturedMovies = webController.RecommendationService.RecommendMoviesForUser(username, 8, RecommendationType.ContentBased);
+                // Get random moviess
+                RandomMovies = webController.MovieService.GetMoviesPage(12, 8);
+                // Get the movies recommended based on the user's behavior
+                UsageBasedMovies = webController.RecommendationService.RecommendMoviesForUser(username, 8, RecommendationType.BehaviorBased);
+                // Get the 8 most recently watched movies
+                var recentlyWatchedMovieIds = webController.UserService.Read(username).RecentlyWatchedMovieIds;
+                ContinueWatching = webController.GetMoviesByIds(recentlyWatchedMovieIds)?.Take(8).ToList();
             }
             catch (System.Exception e)
             {
                 ErrorMessage = "An error occurred while loading the movies: " + e.Message;
             }
+        }
+
+        public IActionResult OnGetWatchMovie(int movieId)
+        {
+            var returnUrl = $"/Streaming?movieId={movieId}";
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/Login", new { ReturnUrl = returnUrl, Message = "You need to authenticate before you can watch a movie." });
+            }
+            return Redirect(returnUrl);
         }
     }
 }
