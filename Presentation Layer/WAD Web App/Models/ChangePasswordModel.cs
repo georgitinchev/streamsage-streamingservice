d83ab095113fb9a4c6a0ff202893ceeba798ce45
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:02:43 +0200

    Different WAD models commit

---
+++ Presentation Layer/WAD Web App/Models/ChangePasswordModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StreamSageWAD.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+    }
+}
