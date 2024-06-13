a087e3730a5cdc1b38ec7f5560456c8e8d5d1511
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:12:43 +0200

    STRATEGY Enum

---
+++ Logic Code Layer/LogicClassLibrary/Enums/RecommendationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClassLibrary.Managers
+{
+    public enum RecommendationType
+    {
+        BehaviorBased,
+        ContentBased
+    }
+}
