65ee00adfb20ba611f3db3737b70a1b6074e6134
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:09:46 +0200

    Int Service Interface 1.0

--- Logic Code Layer/LogicClassLibrary/Interface/Service/IInterpretationService.cs
+++ Logic Code Layer/LogicClassLibrary/Interface/Service/IInterpretationService.cs
@@ -1,7 +1,16 @@
-﻿namespace LogicClassLibrary.Interface.Service
+﻿using DTOs;
+
+namespace LogicClassLibrary.Interface.Service
 {
     public interface IInterpretationService
     {
-        // Add interpretation-related methods here 
+        List<InterpretationDTO> GetInterpretationsPage(int pageNumber, int pageSize);
+        void AddInterpretation(InterpretationDTO interpretation);
+        void UpdateInterpretation(InterpretationDTO interpretation);
+        void DeleteInterpretation(int id);
+        List<InterpretationDTO> GetAllInterpretations();
+        int GetTotalInterpretationsCount();
+        List<InterpretationDTO> GetInterpretationsByMovieId(int movieId);
+        InterpretationDTO GetInterpretationById(int id);
     }
 }
