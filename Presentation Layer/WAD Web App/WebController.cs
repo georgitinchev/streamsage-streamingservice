ccc5c7e97938da27870ee5c843dc64e242e7ea7b
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:00:29 +0200

    Web controller updates

--- Presentation Layer/WAD Web App/WebController.cs
+++ Presentation Layer/WAD Web App/WebController.cs
@@ -1,66 +1,84 @@
-﻿using DesktopApp;
-using DTOs;
+﻿using DTOs;
 using LogicClassLibrary;
 using LogicClassLibrary.Entities;
+using LogicClassLibrary.Interface.Service;
 using LogicClassLibrary.Managers;
 
 namespace StreamSageWAD
 {
-    public class WebController : UserInterfaceWeb
+    public class WebController : IWebController
     {
-        public User? loggedInUser { get; private set; } = null;
-        private string? searchCriteria; // placeholder for search functionality
-        public BackendService backendService;
-        public WebController(User? _loggedInUser)
+        public IMovieService MovieService { get; private set; }
+        public IUserService UserService { get; private set; }
+        public IReviewService ReviewService { get; private set; }
+        public IInterpretationService InterpretationService { get; private set; }
+        public IRecommendationService RecommendationService { get; private set; }
+        public WebController(IMovieService movieService, IUserService userService, IReviewService reviewService, IInterpretationService interpretationService, IRecommendationService recommendationService)
         {
-            loggedInUser = _loggedInUser;
-            searchCriteria = "";
-            backendService = new BackendService();
+            MovieService = movieService;
+            UserService = userService;
+            ReviewService = reviewService;
+            InterpretationService = interpretationService;
+            RecommendationService = recommendationService;
         }
 
-        public WebController()
+        public UserDTO loginUser(string username, string password)
         {
-            searchCriteria = "";
-            backendService = new BackendService();
-        }
-        public void displayHomePage()
-        {
-            // implement
-        }
-
-        public List<Movie> SearchMovies(string criteria)
-        {
-            searchCriteria = criteria;
-            return backendService.SearchMovies(criteria);
-        }
-        public bool UpdateUserProfile(string username, string email, string password, string firstName, string lastName, string profilePic, string setings)
-        {
-            // implement
-            return false;
-        }
-
-        public List<Movie> displayMoviePage()
-        {
-            return backendService.GetAllMovies();
-        }
-
-        public bool loginUser(string username, string password)
-        {
-            bool isAuthenticated = backendService.AuthenticateUser(username, password);
-            if (isAuthenticated)
+            UserService.AuthenticateUser(username, password);
+            var user = UserService.Read(username);
+            if (user == null)
             {
-                loggedInUser = backendService.GetUser(username);
+                throw new StreamSageWAD.Exception.LoginFailedException("User could not be authenticated.");
             }
-            return isAuthenticated;
+            return user;
         }
-        public void registerUser(string username, string password, string email, string firstName, string lastName)
+
+        public UserDTO registerUser(string username, string email, string password, string firstName, string lastName)
         {
-            backendService.RegisterUser(username, email, password, firstName, lastName, "", "");
-            loggedInUser = backendService.GetUser(username);
+            var userSettings = new UserSettingsDTO { Role = "User" };
+            UserService.Create(username, email, password, firstName, lastName, userSettings);
+            var user = UserService.Read(username);
+            if (user == null)
+            {
+                throw new StreamSageWAD.Exception.RegistrationFailedException("User could not be created.");
+            }
+            return user;
         }
+
+        public void changePassword(string username, string newPassword)
+        {
+            UserService.ChangePassword(username, newPassword);
+        }
+
+        public List<MovieDTO> GetMoviesByIds(List<int>? movieIds)
+        {
+            if (movieIds == null)
+            {
+                return null;
+            }
+            if (!movieIds.Any())
+            {
+                return new List<MovieDTO>();
+            }
+            return movieIds.Select(id => MovieService.GetMovie(id)).ToList();
+        }
+
+        (List<ReviewDTO>, List<InterpretationDTO>) GetReviewsAndInterpretationsByMovieId(int movieId)
+        {
+            var reviews = ReviewService.GetReviewsByMovieId(movieId);
+            var interpretations = InterpretationService.GetInterpretationsByMovieId(movieId);
+            return (reviews, interpretations);
+        }
+
+        public string GetDefaultUser()
+        {
+            return "georgi.tin";
+        }
+
         public void logoutUser()
         {
-            loggedInUser = null; // clear the logged-in user
+            // Implement the method
+            throw new NotImplementedException();
         }
     }
 }
