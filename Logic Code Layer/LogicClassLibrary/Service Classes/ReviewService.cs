3dfcdd3bf69ec70183871e207ddb5ad23eb2e288
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:07:16 +0200

    Review Service CRUD 1.0

--- Logic Code Layer/LogicClassLibrary/Service Classes/ReviewService.cs
+++ Logic Code Layer/LogicClassLibrary/Service Classes/ReviewService.cs
@@ -1,6 +1,8 @@
 ﻿using DTOs;
 using LogicClassLibrary.Interface.Manager;
 using LogicClassLibrary.Interface.Service;
+using LogicClassLibrary.Exceptions;
+using System;
 
 namespace LogicClassLibrary.Service_Classes
 {
@@ -15,36 +17,98 @@ namespace LogicClassLibrary.Service_Classes
 
         public List<ReviewDTO> GetAllReviews()
         {
-            return reviewManager.GetAllReviews();
+            try
+            {
+                return reviewManager.GetAllReviews();
+            }
+            catch (Exception ex)
+            {
+                throw new GetAllReviewsError("Error getting all reviews", ex);
+            }
         }
+
         public List<ReviewDTO> GetReviewsPage(int pageNumber, int pageSize)
         {
-            return reviewManager.GetReviewsPage(pageNumber, pageSize);
+            try
+            {
+                return reviewManager.GetReviewsPage(pageNumber, pageSize);
+            }
+            catch (Exception ex)
+            {
+                throw new GetReviewsPageError($"Error getting reviews page {pageNumber}", ex);
+            }
+        }
+
+        public List<ReviewDTO> GetReviewsByMovieId(int movieId)
+        {
+            try
+            {
+                return reviewManager.GetReviewsByMovieId(movieId);
+            }
+            catch (Exception ex)
+            {
+                throw new GetReviewsByMovieIdError($"Error getting reviews for movie {movieId}", ex);
+            }
         }
 
         public void AddReview(ReviewDTO review)
         {
-            reviewManager.Create(review);
+            try
+            {
+                reviewManager.Create(review);
+            }
+            catch (Exception ex)
+            {
+                throw new CreateReviewError("Error adding review", ex);
+            }
         }
 
         public ReviewDTO GetReview(int id)
         {
-            return reviewManager.Read(id);
+            try
+            {
+                return reviewManager.Read(id);
+            }
+            catch (Exception ex)
+            {
+                throw new ReadReviewError($"Error reading review {id}", ex);
+            }
         }
 
         public void UpdateReview(ReviewDTO review)
         {
-            reviewManager.Update(review);
+            try
+            {
+                reviewManager.Update(review);
+            }
+            catch (Exception ex)
+            {
+                throw new UpdateReviewError("Error updating review", ex);
+            }
         }
 
         public void DeleteReview(int id)
         {
-            reviewManager.Delete(id);
+            try
+            {
+                reviewManager.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw new DeleteReviewError($"Error deleting review {id}", ex);
+            }
         }
 
         public int GetTotalReviews()
         {
-            return reviewManager.GetTotalReviews();
+            try
+            {
+                return reviewManager.GetTotalReviews();
+            }
+            catch (Exception ex)
+            {
+                throw new GetTotalReviewsError("Error getting total reviews", ex);
+            }
         }
     }
 }
