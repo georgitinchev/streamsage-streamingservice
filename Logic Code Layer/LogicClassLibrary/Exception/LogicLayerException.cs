ad44c6cdde99aa6036f8700f8aaf777f6d207a06
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:12:34 +0200

    Logic Layer Exception extra exceptions

--- Logic Code Layer/LogicClassLibrary/Exception/LogicLayerException.cs
+++ Logic Code Layer/LogicClassLibrary/Exception/LogicLayerException.cs
@@ -1,69 +1,510 @@
-﻿namespace LogicClassLibrary.Exception
+﻿using System;
+
+namespace LogicClassLibrary.Exceptions
 {
-    // represents errors that occur in the logic layer of the application
-    public class LogicLayerException : System.Exception
+    [Serializable]
+    public class InvalidGenreException : Exception
     {
-        // constructor that takes a message as a parameter 
-        public LogicLayerException(string message) : base(message) { }
-        // constructor that takes a message and an inner exception as parameters
-        public LogicLayerException(string message, System.Exception inner) : base(message, inner) { }
+        public InvalidGenreException() : base() { }
+        public InvalidGenreException(string message) : base(message) { }
+        public InvalidGenreException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"InvalidGenreException: {Message}";
+        }
     }
 
-    public class MovieNotFoundException : LogicLayerException
+    [Serializable]
+    public class UpdateEntityError : Exception
     {
-        public MovieNotFoundException(int movieId)
-            : base($"Movie with ID {movieId} not found.") { }
+        public UpdateEntityError() : base() { }
+        public UpdateEntityError(string message) : base(message) { }
+        public UpdateEntityError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UpdateMovieError: {Message}";
+        }
     }
 
-    public class InvalidGenreException : LogicLayerException
+    [Serializable]
+    public class SearchCriteriaError : Exception
     {
-        public InvalidGenreException()
-            : base("A movie must have 1 to 3 genres") { }
+        public SearchCriteriaError() : base() { }
+        public SearchCriteriaError(string message) : base(message) { }
+        public SearchCriteriaError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"SearchCriteriaError: {Message}";
+        }
     }
 
-    public class PopulatingMoviesError : LogicLayerException
+    [Serializable]
+    public class PopulatingEntityError : Exception
     {
-        public PopulatingMoviesError()
-            : base("Error populating movies.") { }
+        public PopulatingEntityError() : base() { }
+        public PopulatingEntityError(string message) : base(message) { }
+        public PopulatingEntityError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"PopulatingMoviesError: {Message}";
+        }
     }
 
-    public class SearchCriteriaError : LogicLayerException
+    [Serializable]
+    public class DeleteEntityError : Exception
     {
-        public SearchCriteriaError()
-            : base("Search criteria error, criteria must be atleast 5 characters.") { }
+        public DeleteEntityError() : base() { }
+        public DeleteEntityError(int movieId) : base($"Error deleting movie with ID: {movieId}") { }
+        public DeleteEntityError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"DeleteMovieError: {Message}";
+        }
     }
 
-    public class UpdateMovieError : LogicLayerException
+    [Serializable]
+    public class NotFoundException : Exception
     {
-        public UpdateMovieError()
-            : base("Error updating movie.") { }
+        public NotFoundException() : base() { }
+        public NotFoundException(int movieId) : base($"Movie with ID: {movieId} not found") { }
+        public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"MovieNotFoundException: {Message}";
+        }
     }
 
-    public class DeleteMovieError : LogicLayerException
+    [Serializable]
+    public class CreateEntityError : Exception
     {
-        public DeleteMovieError(int id)
-            : base($"Error deleting movie with ID {id}.") { }
-    }
-    public class CreateMovieError : LogicLayerException
-    {
-        public CreateMovieError()
-            : base("Error creating movie.") { }
-    }
-    public class UserNotFoundException : LogicLayerException
-    {
-        public UserNotFoundException(int userId)
-            : base($"User with ID {userId} not found.") { }
+        public CreateEntityError() : base() { }
+        public CreateEntityError(string message) : base(message) { }
+        public CreateEntityError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"CreateMovieError: {Message}";
+        }
     }
 
-    public class ReviewNotFoundException : LogicLayerException
+    [Serializable]
+    public class PaginatorException : Exception
     {
-        public ReviewNotFoundException(int reviewId)
-            : base($"Review with ID {reviewId} not found.") { }
+        public PaginatorException() : base() { }
+        public PaginatorException(string message) : base(message) { }
+        public PaginatorException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"PaginatorException: {Message}";
+        }
     }
 
-    public class InterpretationNotFoundException : LogicLayerException
+    [Serializable]
+    public class InvalidException : Exception
     {
-        public InterpretationNotFoundException(int interpretationId)
-            : base($"Interpretation with ID {interpretationId} not found.") { }
+        public InvalidException() : base() { }
+        public InvalidException(string message) : base(message) { }
+        public InvalidException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"InvalidInterpretationException: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class TransformationException : Exception
+    {
+        public TransformationException() : base() { }
+        public TransformationException(string message) : base(message) { }
+        public TransformationException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"TransformationException: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class ReadEntityError : Exception
+    {
+        public ReadEntityError() : base() { }
+        public ReadEntityError(string message) : base(message) { }
+        public ReadEntityError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"ReadMovieError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetTotalEntitiesError : Exception
+    {
+        public GetTotalEntitiesError() : base() { }
+        public GetTotalEntitiesError(string message) : base(message) { }
+        public GetTotalEntitiesError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetTotalMoviesError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetAllEntitiesError : Exception
+    {
+        public GetAllEntitiesError() : base() { }
+        public GetAllEntitiesError(string message) : base(message) { }
+        public GetAllEntitiesError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetAllMoviesError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class AuthException : Exception
+    {
+        public AuthException() : base() { }
+        public AuthException(string message) : base(message) { }
+        public AuthException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"AuthException: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class RegistrationException : Exception
+    {
+        public RegistrationException() : base() { }
+        public RegistrationException(string message) : base(message) { }
+        public RegistrationException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"RegistrationException: {Message}";
+        }
+    }
+    [Serializable]
+    public class UserServiceException : Exception
+    {
+        public UserServiceException() : base() { }
+        public UserServiceException(string message) : base(message) { }
+        public UserServiceException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UserServiceException: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class UserAuthenticationException : UserServiceException
+    {
+        public UserAuthenticationException() : base() { }
+        public UserAuthenticationException(string message) : base(message) { }
+        public UserAuthenticationException(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UserAuthenticationException: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class ReadReviewError : Exception
+    {
+        public ReadReviewError() : base() { }
+        public ReadReviewError(string message) : base(message) { }
+        public ReadReviewError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"ReadReviewError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetAllReviewsError : Exception
+    {
+        public GetAllReviewsError() : base() { }
+        public GetAllReviewsError(string message) : base(message) { }
+        public GetAllReviewsError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetAllReviewsError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetReviewsPageError : Exception
+    {
+        public GetReviewsPageError() : base() { }
+        public GetReviewsPageError(string message) : base(message) { }
+        public GetReviewsPageError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetReviewsPageError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetReviewsByMovieIdError : Exception
+    {
+        public GetReviewsByMovieIdError() : base() { }
+        public GetReviewsByMovieIdError(string message) : base(message) { }
+        public GetReviewsByMovieIdError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetReviewsByMovieIdError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class CreateReviewError : Exception
+    {
+        public CreateReviewError() : base() { }
+        public CreateReviewError(string message) : base(message) { }
+        public CreateReviewError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"CreateReviewError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class UpdateReviewError : Exception
+    {
+        public UpdateReviewError() : base() { }
+        public UpdateReviewError(string message) : base(message) { }
+        public UpdateReviewError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UpdateReviewError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class DeleteReviewError : Exception
+    {
+        public DeleteReviewError() : base() { }
+        public DeleteReviewError(string message) : base(message) { }
+        public DeleteReviewError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"DeleteReviewError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetTotalReviewsError : Exception
+    {
+        public GetTotalReviewsError() : base() { }
+        public GetTotalReviewsError(string message) : base(message) { }
+        public GetTotalReviewsError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetTotalReviewsError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class ReadInterpretationError : Exception
+    {
+        public ReadInterpretationError() : base() { }
+        public ReadInterpretationError(string message) : base(message) { }
+        public ReadInterpretationError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"ReadInterpretationError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetAllInterpretationsError : Exception
+    {
+        public GetAllInterpretationsError() : base() { }
+        public GetAllInterpretationsError(string message) : base(message) { }
+        public GetAllInterpretationsError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetAllInterpretationsError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetInterpretationsPageError : Exception
+    {
+        public GetInterpretationsPageError() : base() { }
+        public GetInterpretationsPageError(string message) : base(message) { }
+        public GetInterpretationsPageError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetInterpretationsPageError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetInterpretationsByMovieIdError : Exception
+    {
+        public GetInterpretationsByMovieIdError() : base() { }
+        public GetInterpretationsByMovieIdError(string message) : base(message) { }
+        public GetInterpretationsByMovieIdError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetInterpretationsByMovieIdError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class CreateInterpretationError : Exception
+    {
+        public CreateInterpretationError() : base() { }
+        public CreateInterpretationError(string message) : base(message) { }
+        public CreateInterpretationError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"CreateInterpretationError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class UpdateInterpretationError : Exception
+    {
+        public UpdateInterpretationError() : base() { }
+        public UpdateInterpretationError(string message) : base(message) { }
+        public UpdateInterpretationError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UpdateInterpretationError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class DeleteInterpretationError : Exception
+    {
+        public DeleteInterpretationError() : base() { }
+        public DeleteInterpretationError(string message) : base(message) { }
+        public DeleteInterpretationError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"DeleteInterpretationError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetTotalInterpretationsError : Exception
+    {
+        public GetTotalInterpretationsError() : base() { }
+        public GetTotalInterpretationsError(string message) : base(message) { }
+        public GetTotalInterpretationsError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetTotalInterpretationsError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class CreateMovieError : Exception
+    {
+        public CreateMovieError() : base() { }
+        public CreateMovieError(string message) : base(message) { }
+        public CreateMovieError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"CreateMovieError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class UpdateMovieError : Exception
+    {
+        public UpdateMovieError() : base() { }
+        public UpdateMovieError(string message) : base(message) { }
+        public UpdateMovieError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"UpdateMovieError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class DeleteMovieError : Exception
+    {
+        public DeleteMovieError() : base() { }
+        public DeleteMovieError(string message) : base(message) { }
+        public DeleteMovieError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"DeleteMovieError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetMovieError : Exception
+    {
+        public GetMovieError() : base() { }
+        public GetMovieError(string message) : base(message) { }
+        public GetMovieError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetMovieError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetAllMoviesError : Exception
+    {
+        public GetAllMoviesError() : base() { }
+        public GetAllMoviesError(string message) : base(message) { }
+        public GetAllMoviesError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetAllMoviesError: {Message}";
+        }
+    }
+
+    [Serializable]
+    public class GetTotalMoviesError : Exception
+    {
+        public GetTotalMoviesError() : base() { }
+        public GetTotalMoviesError(string message) : base(message) { }
+        public GetTotalMoviesError(string message, Exception innerException) : base(message, innerException) { }
+
+        public override string ToString()
+        {
+            return $"GetTotalMoviesError: {Message}";
+        }
     }
 }
