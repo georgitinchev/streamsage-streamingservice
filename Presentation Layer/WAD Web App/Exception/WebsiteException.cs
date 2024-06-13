c24918e11a9ad3e1af4a1823f64ea5a1e54930b4
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-13 02:02:54 +0200

    Website Exceptions class

--- Presentation Layer/WAD Web App/Exception/WebsiteException.cs
+++ Presentation Layer/WAD Web App/Exception/WebsiteException.cs
@@ -1,6 +1,55 @@
 ﻿namespace StreamSageWAD.Exception
 {
-    public class WebsiteException
+    public class WebsiteException : System.Exception
     {
+        public WebsiteException() { }
+
+        public WebsiteException(string message) : base(message) { }
+
+        public WebsiteException(string message, System.Exception inner) : base(message, inner) { }
+    }
+
+    public class UserNotFoundException : WebsiteException
+    {
+        public UserNotFoundException() { }
+
+        public UserNotFoundException(string message) : base(message) { }
+
+        public UserNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+    }
+
+    public class InvalidCredentialsException : WebsiteException
+    {
+        public InvalidCredentialsException() { }
+
+        public InvalidCredentialsException(string message) : base(message) { }
+
+        public InvalidCredentialsException(string message, System.Exception inner) : base(message, inner) { }
+    }
+
+    public class RegistrationFailedException : WebsiteException
+    {
+        public RegistrationFailedException() { }
+
+        public RegistrationFailedException(string message) : base(message) { }
+
+        public RegistrationFailedException(string message, System.Exception inner) : base(message, inner) { }
+    }
+
+    public class LoginFailedException : WebsiteException
+    {
+        public LoginFailedException() { }
+
+        public LoginFailedException(string message) : base(message) { }
+
+        public LoginFailedException(string message, System.Exception inner) : base(message,inner) { }
+    }
+    public class MovieNotFoundException : WebsiteException
+    {
+        public MovieNotFoundException() { }
+
+        public MovieNotFoundException(string message) : base(message) { }
+
+        public MovieNotFoundException(string message, System.Exception inner) : base(message, inner) { }
     }
 }
